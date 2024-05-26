49873431272d6cb65ba11f5546c0070ae72e6ed6
AlanOrtegaDev <alan.ortega.dev@gmail.com>
2024-05-26 16:50:35 -0400

    Change server

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const authKey = process.env.DEEPL_API_KEY
 const translator = new deepl.Translator(authKey)
 
 app
-  .use(cors())
+  .use(cors('*'))
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
 
